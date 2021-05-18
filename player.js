1a370a99272392a268242393e98ceea14ea18261
aryan1515 <82597641+aryan1515@users.noreply.github.com>
2021-05-18 15:32:52 +0530

    Add files via upload

---
+++ player.js
@@ -0,0 +1,31 @@
+
+class player{
+    
+constructor(x,y){
+
+this.x = x;
+this.y = y;
+this.spt=createsprite(this.x,this.y,50,50);
+this.spt.shapecolor ="orange";
+
+}
+
+move(xdir,ydir){
+this.spt.x +=xdir*grid;
+this.spt.y +=ydir*grid;
+
+}
+
+
+
+
+
+
+}
+       
+
+
+
+
+
+
